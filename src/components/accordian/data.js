4a04861e12fa0d0b056522243c38bd61e8fd24cc
Naoufal <naoufal.ikkou@gmail.com>
2024-04-17 14:27:07 +0100

    New project

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accodion components?",
+        answer: "Acoordion components are user interface elements used for origine"
+    },
+    {
+        id: '2',
+        question: "What are they used for?",
+        answer: "They are commonly employed in various contexts, including FAQs",
+    },
+    {
+        id: '3',
+        question: "Accordion as a musical intrument",
+        answer: "The accordion is a musical intrument with a keyboard and beilo"
+    },
+    {
+        id: '4',
+        question: "Can i create an accordion component with a different",
+        answer: "Yes of course, it is very possible to create an accordion component"
+    },
+];
+
+export default data;
